b972403373acccf7a028ce36a82630ad55f44f41
endezz <r-serega13@yandex.ru>
2021-12-31 23:02:08 +0400

    feat: add list for auth users

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import { observer } from 'mobx-react-lite';
-import React, { FC, useContext, useEffect } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
 import { Context } from '.';
 import LoginForm from './components/LoginForm';
+import { IUser } from './models/IUser';
+import UserService from './services/UserService';
 
 const App: FC = () => {
   const { store } = useContext(Context);
+  const [users, setUsers] = useState<IUser[]>([]);
 
   useEffect(() => {
     if (localStorage.getItem('token')) {
@@ -12,18 +15,38 @@ const App: FC = () => {
     }
   }, []);
 
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   if (store.isLoading) {
-    return <div>Загрузка.......</div>
+    return <div>Загрузка.......</div>;
   }
 
   if (!store.isAuth) {
-    return <LoginForm />;
+    return (
+      <div>
+        <LoginForm />
+        <button onClick={getUsers}>Получить список пользователей</button>
+      </div>
+    );
   }
 
   return (
     <div>
       <h1>{store.isAuth ? `Пользователь авторизован ${store.user.email}` : 'Авторизуйтесь!'}</h1>
       <button onClick={() => store.logout()}>Выйти</button>
+      <div>
+        <button onClick={getUsers}>Получить список пользователей</button>
+      </div>
+      {users.map((user) => (
+        <div key={user.email}>{user.email}</div>
+      ))}
     </div>
   );
 };
