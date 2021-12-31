8a178863e6ab4ffcdfc078353a837955373319fb
endezz <r-serega13@yandex.ru>
2021-12-31 20:43:35 +0400

    feat: add request validation

--- server/router/index.js
+++ server/router/index.js
@@ -1,8 +1,15 @@
 const ExpressRouter = require('express').Router;
-const userController = require('../controllers/user-controller')
+const userController = require('../controllers/user-controller');
 const router = new ExpressRouter();
+const expressValidator = require('express-validator');
 
-router.post('/registration', userController.registration);
+router.post(
+  '/registration',
+  expressValidator.body('email').isEmail(),
+  expressValidator.body('password').isLength({ min: 3, max: 32 }),
+  userController.registration
+);
+// router.post('/registration', userController.registration);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
