41b1e2302d800486b6877c99391f1039b4bf3a0e
endezz <r-serega13@yandex.ru>
2021-12-31 22:35:42 +0400

    feat: add frontend logic

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,20 @@
+import { FC, useContext, useState } from 'react';
+import { Context } from '..';
+
+const LoginForm: FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const { store } = useContext(Context);
+
+  return (
+    <div>
+      <input type="text" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" value={password} placeholder="Пароль" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={() => store.login(email, password)}>Логин</button>
+      <button onClick={() => store.registration(email, password)}>Регистрация</button>
+    </div>
+  );
+};
+
+export default LoginForm;
